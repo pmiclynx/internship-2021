30c2fee04f4aca3e073ba5e80e638600eb8196a9
ionelchis <ionel.chis00@gmail.com>
2021-08-03 15:29:28 +0300

    Merged bottom nav, splash screen changes

--- app/src/main/java/com/internship/tvseries/MainActivity.java
+++ app/src/main/java/com/internship/tvseries/MainActivity.java
@@ -2,7 +2,6 @@ package com.internship.tvseries;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;

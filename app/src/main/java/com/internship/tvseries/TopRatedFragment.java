dd06cb99cc938af03e3880790744cd8093185656
ionelchis <ionel.chis00@gmail.com>
2021-08-03 12:27:03 +0300

    updated the bottom nav with Top Rated tab

--- app/src/main/java/com/internship/tvseries/TopRatedFragment.java
+++ app/src/main/java/com/internship/tvseries/TopRatedFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LatestFragment#newInstance} factory method to
+ * Use the {@link TopRatedFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LatestFragment extends Fragment {
+public class TopRatedFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class LatestFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LatestFragment() {
+    public TopRatedFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class LatestFragment extends Fragment {
      * @return A new instance of fragment LatestFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LatestFragment newInstance(String param1, String param2) {
-        LatestFragment fragment = new LatestFragment();
+    public static TopRatedFragment newInstance(String param1, String param2) {
+        TopRatedFragment fragment = new TopRatedFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class LatestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_latest, container, false);
+        return inflater.inflate(R.layout.fragment_top_rated, container, false);
     }
 }

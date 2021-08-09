09593cde0e62421ad17c1d0f13938fe32c569bb9
ionelchis <ionel.chis00@gmail.com>
2021-08-09 13:49:59 +0300

    added retrofit logging

--- app/src/main/java/com/internship/tvseries/data/api/ApiClient.java
+++ app/src/main/java/com/internship/tvseries/data/api/ApiClient.java
@@ -2,21 +2,19 @@ package com.internship.tvseries.data.api;
 
 import com.internship.tvseries.utils.Constants;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.HttpUrl;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class ApiClient {
+
+    private static final HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC);
 
     private static final OkHttpClient okHttpClient = new OkHttpClient.Builder()
             .addInterceptor(chain -> {
@@ -32,6 +30,7 @@ public class ApiClient {
                         .build();
                 return chain.proceed(request);
             })
+            .addInterceptor(loggingInterceptor)
             .connectTimeout(60, TimeUnit.SECONDS)
             .build();
 
@@ -42,6 +41,7 @@ public class ApiClient {
             .build();
 
     private static final TvDetailsApi tvDetailsApi = retrofit.create(TvDetailsApi.class);
+
     public static TvDetailsApi getTvDetailsApi() {
         return tvDetailsApi;
     }

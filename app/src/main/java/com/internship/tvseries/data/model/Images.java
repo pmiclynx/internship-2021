23bce0f1ef0f7330391a90c68c83212369d6c710
ionelchis <ionel.chis00@gmail.com>
2021-08-09 12:40:32 +0300

    added configuration class for images

---
+++ app/src/main/java/com/internship/tvseries/data/model/Images.java
@@ -0,0 +1,56 @@
+package com.internship.tvseries.data.model;
+
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+public class Images{
+
+	@SerializedName("poster_sizes")
+	private List<String> posterSizes;
+
+	@SerializedName("secure_base_url")
+	private String secureBaseUrl;
+
+	@SerializedName("backdrop_sizes")
+	private List<String> backdropSizes;
+
+	@SerializedName("base_url")
+	private String baseUrl;
+
+	@SerializedName("logo_sizes")
+	private List<String> logoSizes;
+
+	@SerializedName("still_sizes")
+	private List<String> stillSizes;
+
+	@SerializedName("profile_sizes")
+	private List<String> profileSizes;
+
+	public List<String> getPosterSizes(){
+		return posterSizes;
+	}
+
+	public String getSecureBaseUrl(){
+		return secureBaseUrl;
+	}
+
+	public List<String> getBackdropSizes(){
+		return backdropSizes;
+	}
+
+	public String getBaseUrl(){
+		return baseUrl;
+	}
+
+	public List<String> getLogoSizes(){
+		return logoSizes;
+	}
+
+	public List<String> getStillSizes(){
+		return stillSizes;
+	}
+
+	public List<String> getProfileSizes(){
+		return profileSizes;
+	}
+}

8e65c313e68c56c02a7db0ea3c3af7fb9a1d6752
rafaipaula <rafai.paula3012@gmail.com>
2021-08-12 13:13:26 +0300

    favorites-vieemodelfactory

---
+++ app/src/main/java/com/internship/tvseries/ui/favorites/FavoritesViewModelFactory.java
@@ -0,0 +1,23 @@
+package com.internship.tvseries.ui.favorites;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.internship.tvseries.data.repository.favorites.FavoritesRepository;
+
+public class FavoritesViewModelFactory implements ViewModelProvider.Factory {
+    private final FavoritesRepository favoritesRepository;
+
+    public FavoritesViewModelFactory(FavoritesRepository favoritesRepository){
+        this.favoritesRepository = favoritesRepository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(FavoritesViewModel.class))
+            return (T) new FavoritesViewModel(favoritesRepository);
+        throw new IllegalArgumentException("Unknown View Model Class");
+    }
+}

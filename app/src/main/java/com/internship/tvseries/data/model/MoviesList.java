f846b02ce1f3df319ce9a4653f96dfe09faec945
ioana <ioanacarnaru@yahoo.com>
2021-08-16 16:15:43 +0300

    Lynx Top Rated Repository

--- app/src/main/java/com/internship/tvseries/data/model/MoviesList.java
+++ app/src/main/java/com/internship/tvseries/data/model/MoviesList.java
@@ -11,17 +11,11 @@ import java.util.List;
 public class MoviesList implements Serializable {
 
 
-    @SerializedName("page")
-    private Integer page;
 
     @SerializedName("results")
     public List<Result> results = null;
 
-    @SerializedName("total_pages")
-    private Integer totalPages;
 
-    @SerializedName("total_results")
-    private Integer totalResults;
 
     private final static long serialVersionUID = 7334358210326409107L;
 
@@ -29,26 +23,13 @@ public class MoviesList implements Serializable {
     public MoviesList() {
     }
 
-    public MoviesList(Integer page, List<Result> results, Integer totalPages, Integer totalResults) {
+    public MoviesList(List<Result> results) {
         super();
-        this.page = page;
         this.results = results;
-        this.totalPages = totalPages;
-        this.totalResults = totalResults;
     }
 
-    public Integer getPage() {
-        return page;
-    }
 
-    public void setPage(Integer page) {
-        this.page = page;
-    }
 
-    public MoviesList withPage(Integer page) {
-        this.page = page;
-        return this;
-    }
 
     public List<Result> getResults() {
         return results;
@@ -63,31 +44,7 @@ public class MoviesList implements Serializable {
         return this;
     }
 
-    public Integer getTotalPages() {
-        return totalPages;
-    }
 
-    public void setTotalPages(Integer totalPages) {
-        this.totalPages = totalPages;
-    }
-
-    public MoviesList withTotalPages(Integer totalPages) {
-        this.totalPages = totalPages;
-        return this;
-    }
-
-    public Integer getTotalResults() {
-        return totalResults;
-    }
-
-    public void setTotalResults(Integer totalResults) {
-        this.totalResults = totalResults;
-    }
-
-    public MoviesList withTotalResults(Integer totalResults) {
-        this.totalResults = totalResults;
-        return this;
-    }
 
 
 }
--- app/src/main/java/com/internship/tvseries/data/repository/backend/LynxTopRatedRepository.java
+++ app/src/main/java/com/internship/tvseries/data/repository/backend/LynxTopRatedRepository.java
@@ -1,17 +1,56 @@
 package com.internship.tvseries.data.repository.backend;
 
+import android.util.Log;
+
+import com.internship.tvseries.data.api.LynxApiClient;
+import com.internship.tvseries.data.api.MovieApi;
+import com.internship.tvseries.data.model.MoviesList;
 import com.internship.tvseries.data.model.Result;
 import com.internship.tvseries.data.repository.TvRepository;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class LynxTopRatedRepository implements TvRepository {
+
+    private static LynxTopRatedRepository instance = null;
+    private MovieApi api;
+
+    public LynxTopRatedRepository(MovieApi api) {
+        this.api = api;
+    }
+
+    public static LynxTopRatedRepository getInstance(MovieApi api) {
+        if (instance == null)
+            instance = new LynxTopRatedRepository(api);
+        return instance;
+    }
+
     @Override
     public void getByCategory(String category, Consumer<List<Result>> consumer) {
+        Call<MoviesList> call = instance.api.listOfMovies(category);
+        call.enqueue(new Callback<MoviesList>() {
+            @Override
+            public void onResponse(@NotNull Call<MoviesList> call, @NotNull Response<MoviesList> response) {
+                if (response.code() == 200) {
+                    MoviesList movies = response.body();
+                    if (movies != null) {
+                        consumer.accept(movies.getResults());
+                    }
+                }
+            }
 
-
-        consumer.accept(new ArrayList<>());
+            @Override
+            public void onFailure(Call<MoviesList> call, Throwable t) {
+                consumer.accept(new ArrayList<>());
+            }
+        });
     }
 }
